8ce29cbd1348cbb6570e3378473c63ca1414ccf4
Lương Vũ <90044650+Dailuong11@users.noreply.github.com>
2023-02-23 00:52:07 +0700

    item_search
    update item

--- SimpleNote-main/app/src/main/java/com/groupx/simplenote/activity/AchievementActivity.java
+++ SimpleNote-main/app/src/main/java/com/groupx/simplenote/activity/AchievementActivity.java
@@ -13,5 +13,6 @@ public class AchievementActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_achievement);
         //aaaaaaaaaaaaaaaaaaaaa
+
     }
 }
